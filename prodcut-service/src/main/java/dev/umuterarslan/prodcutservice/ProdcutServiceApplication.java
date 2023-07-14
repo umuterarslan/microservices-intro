7f198fe3559b76e0ff19c7c56c2db9a75f2df11b
umuterarslan <eumuterarslan@gmail.com>
2023-07-14 16:53:53 +0300

    [product-service] init

---
+++ prodcut-service/src/main/java/dev/umuterarslan/prodcutservice/ProdcutServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.umuterarslan.prodcutservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdcutServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdcutServiceApplication.class, args);
+    }
+
+}
---
+++ prodcut-service/src/test/java/dev/umuterarslan/prodcutservice/ProdcutServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.umuterarslan.prodcutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdcutServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

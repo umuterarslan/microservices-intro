43a4011a7e1f518d7ea7ebf0086760c5f0b0cf41
umuterarslan <eumuterarslan@gmail.com>
2023-08-04 00:25:51 +0300

    [product-service] add response dtos

---
+++ prodcut-service/src/main/java/dev/umuterarslan/prodcutservice/dto/response/UpdateProductResponse.kt
@@ -0,0 +1,12 @@
+package dev.umuterarslan.prodcutservice.dto.response
+
+import java.util.*
+
+data class UpdateProductResponse(
+        val id: UUID,
+        val name: String,
+        val description: String,
+        val price: Double,
+        val createdAt: Date,
+        val updatedAt: Date
+)

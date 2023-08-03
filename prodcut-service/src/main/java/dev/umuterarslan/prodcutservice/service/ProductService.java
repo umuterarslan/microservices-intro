5d2439cab929413730099aa14831abdf66c627b7
umuterarslan <eumuterarslan@gmail.com>
2023-08-04 00:15:54 +0300

    [product-service] add ProductService

---
+++ prodcut-service/src/main/java/dev/umuterarslan/prodcutservice/service/ProductService.java
@@ -0,0 +1,31 @@
+package dev.umuterarslan.prodcutservice.service;
+
+import dev.umuterarslan.prodcutservice.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    public ProductService(ProductRepository repository) {
+    }
+
+    public CreateProductResponse createProduct(CreateProductRequest request) {
+        return null;
+    }
+
+    public GetProductByIdResponse getProductById(String id) {
+        return null;
+    }
+
+    public GetProductsPaginatedResponse getProductsPaginated() {
+        return null;
+    }
+
+    public UpdateProductResponse updateProduct(UpdateProductRequest request) {
+        return null;
+    }
+
+    public DeleteProductByIdResponse deleteProductById(String id) {
+        return null;
+    }
+}

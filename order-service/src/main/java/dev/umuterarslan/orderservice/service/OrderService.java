444d564c0573f37ade7b5e44795a1a0a62b3792e
umuterarslan <eumuterarslan@gmail.com>
2023-10-11 21:03:19 +0300

    [order-service] add stock check to createOrder method

--- order-service/src/main/java/dev/umuterarslan/orderservice/service/OrderService.java
+++ order-service/src/main/java/dev/umuterarslan/orderservice/service/OrderService.java
@@ -3,15 +3,18 @@ package dev.umuterarslan.orderservice.service;
 import dev.umuterarslan.orderservice.dto.order.requests.CreateOrderRequest;
 import dev.umuterarslan.orderservice.dto.order.responses.CreateOrderResponse;
 import dev.umuterarslan.orderservice.dto.order.responses.GetOrdersByUserIdPaginatedResponse;
+import dev.umuterarslan.orderservice.dto.orderitem.requests.CreateOrderItemRequest;
 import dev.umuterarslan.orderservice.mapper.OrderServiceMapper;
 import dev.umuterarslan.orderservice.model.Order;
 import dev.umuterarslan.orderservice.model.OrderItem;
 import dev.umuterarslan.orderservice.repository.OrderRepository;
+import dev.umuterarslan.orderservice.service.rules.OrderServiceBusinessRules;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Date;
 import java.util.List;
@@ -22,14 +25,18 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final OrderServiceMapper orderServiceMapper;
     private final OrderItemService orderItemService;
+    private final OrderServiceBusinessRules rules;
 
-    public OrderService(OrderRepository orderRepository, OrderServiceMapper orderServiceMapper, OrderItemService orderItemService) {
+    public OrderService(OrderRepository orderRepository, OrderServiceMapper orderServiceMapper, OrderItemService orderItemService, OrderServiceBusinessRules rules) {
         this.orderRepository = orderRepository;
         this.orderServiceMapper = orderServiceMapper;
         this.orderItemService = orderItemService;
+        this.rules = rules;
     }
 
     public CreateOrderResponse createOrder(CreateOrderRequest request) {
+        request.getItems().forEach(rules::checkIfProductQuantityBiggerThanStock);
+
         Order saved = orderRepository.save(
                 new Order(
                         UUID.randomUUID(),
